89228d7373c5cd773df009a7b98ada411155d055
angelomira <57592842+Falcion@users.noreply.github.com>
2024-03-31 14:54:43 +0300

    feat!(code): global rework of prepare script

--- prepare.ts
+++ prepare.ts
@@ -15,136 +15,119 @@
 import * as fs from 'fs-extra';
 import * as path from 'path';
 import * as dotenv from 'dotenv';
+
 import readline from 'readline';
 
 import colors from 'colors/safe';
 
-const ROOT_DIRECTORY = __dirname;
+class PREPARE_MODULE {
+    ROOT_DIRECTORY: string = __dirname;
 
-const EXCLUDING_DIRECTORIES = ['node_modules', 'venv', '.git', 'out'];
+    EXCLUDING_FOLDER: string[] = ['node_modules', 'venv', '.git', 'out'];
+    EXCLUDING_VALUES: string[] = ['FALCION',
+        'PATTERNU',
+        'PATTERNUGIT'];
 
-let TARGET_VALUES = ['FALCION', 'PATTERNU', 'PATTERNUGIT'];
-
-(async () => {
-  /**
-   * Searches for specific strings in a file.
-   * @param {string} filePath - The path to the file to search in.
-   * @param {string[]} searchData - The strings to search for.
-   */
-  const searchData = async (filePath: string, searchData: string[]) => {
-    const content = (await fs.readFile(filePath, 'utf-8')).split('\n');
-
-    for (let i = 0; i < content.length; i++) {
-      const line = content[i].toUpperCase();
-
-      for (const searchingString of searchData) {
-        if (line.includes(searchingString)) {
-          console.log(colors.green(`Found "${searchingString}" in L#${i} of:\n` + colors.blue(`"${filePath}"`)));
-        }
-      }
+    constructor(entries: string[]) {
+        if (entries[0] != 'NO')
+            for (const item in entries)
+                this.EXCLUDING_VALUES.push(item);
+        else
+            this.EXCLUDING_VALUES = this.EXCLUDING_VALUES;
     }
-  };
 
-  /**
-   * Traverses directories recursively and searches for target values.
-   * @param {string} dirPath - The path to the directory to traverse.
-   */
-  const traverseDir = async (dirPath: string) => {
-    try {
-      const files = await fs.readdir(dirPath);
+    async search(filepath: string,
+        data: string[]) {
+        const content = (await fs.readFile(filepath, 'utf-8')).split('\n');
 
-      for (const file of files) {
-        const filePath = path.join(dirPath, file);
-        const fileStat = await fs.stat(filePath);
+        for (let i = 0; i < content.length; i++) {
+            const line = content[i].toUpperCase();
 
-        if (fileStat.isDirectory()) {
-          if (!EXCLUDING_DIRECTORIES.includes(file)) {
-            await traverseDir(filePath);
-          }
-        } else if (fileStat.isFile()) {
-          await searchData(filePath, TARGET_VALUES);
+            for (const target of data)
+                if (line.includes(target))
+                    console.info(colors.green(`Found "${target}" in L#${i} of:\n` + colors.cyan(filepath)))
         }
-      }
-    } catch (err) {
-      console.error(`Error reading directory ${dirPath}: ${err}`);
     }
-  };
 
-  /**
-   * Writes manifest data to a file.
-   * @param {any} json - The data (in JSON) to write to the manifest.
-   * @param {string} authorUrl - The URL of the author's profile.
-   */
-  const writeManifest = async (json: any, authorUrl: string | undefined) => {
-    const manifestJSON = {
-      id: json.name,
-      name: `${json.name[0].toUpperCase()}${json.name.slice(1)}`,
-      description: json.description,
-      author: json.author,
-      license: json.license,
-      version: json.version,
-      authorUrl: authorUrl === undefined ? '-' : authorUrl
+    async traverse(directory: string) {
+        try {
+            const files: string[] = await fs.readdir(directory);
+
+            for (const file in files) {
+                const filepath = path.join(directory, file);
+
+                const filestat = await fs.stat(filepath);
+
+
+                if (filestat.isDirectory())
+                    if (!this.EXCLUDING_FOLDER.includes(file))
+                        await this.traverse(filepath);
+                    else if (filestat.isFile())
+                        await this.search(filepath, this.EXCLUDING_VALUES);
+                    else
+                        throw new Error(colors.red('No correct data was found, exception attribute is:') + colors.bgRed(` ${filepath}`));
+            }
+        } catch (err) { console.error(colors.red('Error via reading given directory: ' + `${err}`)); }
+    }
+}
+
+fs.ensureFileSync(path.join(__dirname, '.env'));
+fs.ensureFileSync(path.join(__dirname, 'manifest.json'));
+
+fs.writeFileSync(path.join(__dirname, '.env'), 'EXAMPLE_API_KEY=');
+fs.writeFileSync(path.join(__dirname, 'manifest.json'), JSON.stringify({}, undefined, 4));
+
+dotenv.config({
+    path: '.env',
+    encoding: 'utf-8'
+});
+
+const PACKAGE_JSON: any = JSON.parse(fs.readFileSync('package.json', { encoding: 'utf-8' }));
+
+const MANIFEST: any = JSON.parse(fs.readFileSync('manifest.json', { encoding: 'utf-8' }));
+
+if (PACKAGE_JSON.name === MANIFEST.id &&
+    PACKAGE_JSON.displayName === MANIFEST.name &&
+    PACKAGE_JSON.description === MANIFEST.description &&
+    PACKAGE_JSON.author.name === MANIFEST.author &&
+    PACKAGE_JSON.author.url === MANIFEST.authorUrl &&
+    PACKAGE_JSON.license === MANIFEST.license &&
+    PACKAGE_JSON.version === MANIFEST.version) {
+    console.warn(colors.bgGreen(colors.white('Manifest is synced with package, keep everything as it was.')));
+}
+else {
+    console.warn(colors.bgBlue(colors.yellow('Manifest is not synced with package\'s information, rewriting it.')));
+
+    fs.copyFileSync('manifest.json', 'manifest-backup.json');
+
+    const input_json: any = {
+        id: PACKAGE_JSON.name,
+        name: PACKAGE_JSON.displayName,
+        description: PACKAGE_JSON.description,
+        author: PACKAGE_JSON.author.name,
+        authorUrl: PACKAGE_JSON.author.url,
+        license: PACKAGE_JSON.license,
+        version: PACKAGE_JSON.version,
     };
 
-    await fs.writeFile('manifest.json', JSON.stringify(manifestJSON, null, 4));
-  };
+    fs.writeFileSync('manifest.json', JSON.stringify(input_json, undefined, 4));
+}
 
-  if (!(await fs.pathExists(path.join(ROOT_DIRECTORY, '.env')))) {
-    await fs.createFile('.env');
-    await fs.writeFile('.env', 'EXAMPLE_API_KEY=');
-  }
+const RL = readline.createInterface({ input: process.stdin, output: process.stdout });
 
-  dotenv.config({
-    path: './.env',
-    encoding: 'utf-8'
-  });
+RL.question(colors.bold('Change finding signatures (words) for the finder script? [Y/N]: '), (ASW1) => {
+    if (ASW1.toUpperCase() == 'Y') {
+        RL.question(colors.bold('Enter your custom signatures (words) separatedly by commas: '), (ASW2) => {
+            const input: string[] = ASW2.split(',');
 
-  const packageJSON = JSON.parse(fs.readFileSync('package.json', { encoding: 'utf-8' }));
+            new PREPARE_MODULE(input).traverse(__dirname);
 
-  if (!(await fs.pathExists(path.join(ROOT_DIRECTORY, 'manifest.json')))) {
-    await fs.createFile('manifest.json');
-    await writeManifest(packageJSON, undefined);
-  }
-
-  const manifestAsJSON = JSON.parse(fs.readFileSync('manifest.json', { encoding: 'utf-8' }));
-
-  let authorUrl = manifestAsJSON.authorUrl;
-
-  if (authorUrl === '-') {
-    authorUrl = undefined;
-  }
-
-  const checkingRes = packageJSON.id === manifestAsJSON.id
-    && packageJSON.name === manifestAsJSON.name
-    && packageJSON.description === manifestAsJSON.description
-    && packageJSON.author === manifestAsJSON.author
-    && packageJSON.license === manifestAsJSON.license
-    && packageJSON.version === manifestAsJSON.version;
-
-  if (!checkingRes) {
-    console.error(colors.red('There is desync in Manifest JSON and Package JSON! Causing override to manifest.json, but backing up at manifest-copy.json'));
-  }
-
-  await fs.copyFile('manifest.json', 'manifest-backup.json');
-
-  await writeManifest(packageJSON, authorUrl);
-
-  const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-  });
-
-  rl.question(colors.yellow('Do you want to change the finding signature for the script? (y/n): '), (answ1) => {
-    if (answ1 === 'y') {
-      rl.question(colors.yellow('What words do you need to find? (separated by comma): '), (answ2) => {
-        const res = answ2.split(',');
-        TARGET_VALUES = res;
-        traverseDir(ROOT_DIRECTORY);
-        rl.close();
-      });
-    } else {
-      traverseDir(ROOT_DIRECTORY);
-      rl.close();
+            RL.close();
+        })
     }
-  });
-})();
+    else {
+        new PREPARE_MODULE(['NO']).traverse(__dirname);
+
+        RL.close();
+    }
+});
