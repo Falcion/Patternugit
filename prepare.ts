bd576b490304c6bc4bf45207f6790d85fc623d8d
Falcion <57592842+Falcion@users.noreply.github.com>
2024-04-21 20:49:15 +0300

    feat(full-revamp-script): now prepare module is revampled

--- prepare.ts
+++ prepare.ts
@@ -12,36 +12,83 @@
  * @year 2023-2024
  */
 
-import * as fs from 'fs-extra';
 import * as path from 'path';
+import * as fsxt from 'fs-extra';
 import * as dotenv from 'dotenv';
-
-import readline from 'readline';
 
 import colors from 'colors/safe';
 
-class PREPARE_MODULE {
-    ROOT_DIRECTORY: string = __dirname;
+import readline from 'readline';
 
-    EXCLUDING_FOLDER: string[] = ['node_modules', 'venv', '.git', 'out'];
-    EXCLUDING_VALUES: string[] = [
+/**
+ * Class representing a module for preparing and managing project files.
+ * This module includes functionality for searching directories, updating manifests, and more.
+ * @class
+ */
+class PREPARE_MODULE {
+    /**
+     * The root directory of the project.
+     * @type {string}
+     */
+    public ROOT_DIRECTORY: string = __dirname;
+
+    /**
+     * Array of folder names to exclude from directory traversal.
+     * @type {string[]}
+     */
+    private EXCLUDING_FOLDER: string[] = [
+        'node_modules',
+        'venv',
+        '.git',
+        'out'
+    ];
+    /**
+     * Array of values to include into file content search.
+     * @type {string[]}
+     */
+    private EXCLUDING_VALUES: string[] = [
         'FALCION',
         'PATTERNU',
         'PATTERNUGIT'
     ];
 
-    constructor(entries: string[]) {
-        if (entries[0] != 'NO')
+    /**
+     * Flag indicating whether to update the manifesto file.
+     * @type {boolean}
+     */
+    public UPDATE_MANIFESTO?: boolean = false;
+
+    /**
+     * Creates an instance of the `PREPARE_MODULE` class.
+     * @constructor
+     * @param {string[]} entries - Custom signatures (words) for file content search.
+     * @param {string} extraction_mode - Extraction mode: 'Y' for appending, 'N' for replacing, 'C' for custom.
+     */
+    constructor(entries: string[], extraction_mode: string = 'C') {
+        if (extraction_mode == 'Y') {
             for (const item of entries)
                 this.EXCLUDING_VALUES.push(item);
+        }
+        if (extraction_mode == 'N') {
+            this.EXCLUDING_VALUES = this.EXCLUDING_VALUES;
+        }
+        if (extraction_mode == 'C') {
+            this.EXCLUDING_VALUES = entries;
+        }
     }
 
-    async search(filepath: string,
-        data: string[]) {
-        const content = (await fs.readFile(filepath, 'utf-8')).split('\n');
+    /**
+     * Asynchronously searches for custom signatures (words) in the content of a file.
+     * @param {string} filepath - The path of the file to search.
+     * @param {string[]} data - Custom signatures (words) to search for.
+     * @returns {Promise<void>}
+     */
+    async __searchDir(filepath: string, data: string[]): Promise<void> {
+        const filedata: string = await fsxt.readFile(filepath, 'utf-8');
+        const contents: string[] = filedata.split('\n');
 
-        for (let i = 0; i < content.length; i++) {
-            const line = content[i].toUpperCase();
+        for (let i = 0; i < contents.length; i++) {
+            const line = contents[i].toUpperCase();
 
             for (const target of data)
                 if (line.includes(target))
@@ -49,83 +96,145 @@ class PREPARE_MODULE {
         }
     }
 
-    async traverse(directory: string) {
+    /**
+     * Asynchronously traverses a directory, searching for files and directories.
+     * @param {string} directory - The directory to traverse.
+     * @returns {Promise<void>}
+     */
+    async __traverDir(directory: string): Promise<void> {
         try {
-            const files: string[] = await fs.readdir(directory);
+            const files: string[] = await fsxt.readdir(directory);
 
             for (const file of files) {
                 const filepath = path.join(directory, file);
 
-                const filestat = await fs.stat(filepath);
+                const filestat = await fsxt.stat(filepath);
 
                 if (filestat.isDirectory()) {
                     if (!this.EXCLUDING_FOLDER.includes(file)) {
-                        await this.traverse(filepath);
+                        await this.__traverDir(filepath);
                     }
-                } else
-                    await this.search(filepath, this.EXCLUDING_VALUES);
+                } else if (filestat.isFile()) {
+                    await this.__searchDir(filepath, this.EXCLUDING_VALUES);
+                }
+                else
+                    continue;
             }
         }
-        catch (err) { console.error(colors.red('Error via reading given directory: ' + `${err}`)); }
+        catch (err) {
+            console.error(colors.red('Error via reading given directory: ' + `${err}`));
+        }
+    }
+
+    /**
+     * Asynchronously prepares project files, such as .env and manifest.json.
+     * @returns {Promise<void>}
+     */
+    async __prpPrjct(): Promise<void> {
+        const input1: string =
+            [
+                '# Type here any requested keys, token or other, for example, API or connection data',
+                'EXAMPLE_API_KEY='
+            ]
+                .join('\n');
+
+        const input2: string = JSON.stringify({}, undefined, 2);
+
+        if (await fsxt.exists(path.join(__dirname, '.env'))) {
+            await fsxt.ensureFile(path.join(__dirname, '.env'));
+            await fsxt.writeFile(path.join(__dirname, '.env'),
+                input1);
+        }
+
+        if (await fsxt.exists(path.join(__dirname, 'manifest.json'))) {
+            await fsxt.ensureFile(path.join(__dirname, 'manifest.json'));
+            await fsxt.writeFile(path.join(__dirname, 'manifest.json'),
+                input2);
+        }
+
+        if (this.UPDATE_MANIFESTO == undefined || this.UPDATE_MANIFESTO == false) {
+            return;
+        }
+
+        const PACKAGER: any = JSON.parse(await fsxt.readFile('package.json', { encoding: 'utf-8' }));
+        const MANIFEST: any = JSON.parse(await fsxt.readFile('manifest.json', { encoding: 'utf-8' }));
+
+        if (PACKAGER.name === MANIFEST.id &&
+            PACKAGER.displayName === MANIFEST.name &&
+            PACKAGER.description === MANIFEST.description &&
+            PACKAGER.author.name === MANIFEST.author &&
+            PACKAGER.author.url === MANIFEST.authorUrl &&
+            PACKAGER.license === MANIFEST.license &&
+            PACKAGER.version === MANIFEST.version) {
+            console.warn(colors.bgGreen(colors.white('Manifest is synced with package, keep everything as it was.')));
+        }
+        else {
+            console.warn(colors.bgBlue(colors.yellow('Manifest is not synced with package\'s information, rewriting it.')));
+
+            await fsxt.copyFile(
+                'manifest.json',
+                'manifest-backup.json');
+
+            const data: any = {
+                'id':
+                    PACKAGER.name,
+                'name':
+                    PACKAGER.displayName,
+                'description':
+                    PACKAGER.description,
+                'author':
+                    PACKAGER.author.name,
+                'authorUrl':
+                    PACKAGER.author.url,
+                'license':
+                    PACKAGER.license,
+                'version':
+                    PACKAGER.version,
+            };
+
+            await fsxt.writeFile('manifest.json', JSON.stringify(data, undefined, 2));
+        }
     }
 }
-
-fs.ensureFileSync(path.join(__dirname, '.env'));
-fs.ensureFileSync(path.join(__dirname, 'manifest.json'));
-
-fs.writeFileSync(path.join(__dirname, '.env'), 'EXAMPLE_API_KEY=');
-fs.writeFileSync(path.join(__dirname, 'manifest.json'), JSON.stringify({}, undefined, 4));
 
 dotenv.config({
     path: '.env',
     encoding: 'utf-8'
 });
 
-const PACKAGE_JSON: any = JSON.parse(fs.readFileSync('package.json', { encoding: 'utf-8' }));
-
-const MANIFEST: any = JSON.parse(fs.readFileSync('manifest.json', { encoding: 'utf-8' }));
-
-if (PACKAGE_JSON.name === MANIFEST.id &&
-    PACKAGE_JSON.displayName === MANIFEST.name &&
-    PACKAGE_JSON.description === MANIFEST.description &&
-    PACKAGE_JSON.author.name === MANIFEST.author &&
-    PACKAGE_JSON.author.url === MANIFEST.authorUrl &&
-    PACKAGE_JSON.license === MANIFEST.license &&
-    PACKAGE_JSON.version === MANIFEST.version) {
-    console.warn(colors.bgGreen(colors.white('Manifest is synced with package, keep everything as it was.')));
-}
-else {
-    console.warn(colors.bgBlue(colors.yellow('Manifest is not synced with package\'s information, rewriting it.')));
-
-    fs.copyFileSync('manifest.json', 'manifest-backup.json');
-
-    const input_json: any = {
-        'id': 		PACKAGE_JSON.name,
-        'name': 	PACKAGE_JSON.displayName,
-        'description': PACKAGE_JSON.description,
-        'author': 	PACKAGE_JSON.author.name,
-        'authorUrl': PACKAGE_JSON.author.url,
-        'license': 	PACKAGE_JSON.license,
-        'version': 	PACKAGE_JSON.version,
-    };
-
-    fs.writeFileSync('manifest.json', JSON.stringify(input_json, undefined, 4));
-}
-
 const RL = readline.createInterface({ input: process.stdin, output: process.stdout });
 
-RL.question(colors.bold('Change finding signatures (words) for the finder script? [Y/N]: '), (ASW1) => {
-    if (ASW1.toUpperCase() == 'Y') {
-        RL.question(colors.bold('Enter your custom signatures (words) separatedly by commas: '), (ASW2) => {
-            const input: string[] = ASW2.split(',');
+RL.question(colors.bold('Change signatures of manifesto (JSON) for the repository [Y/N]: '), (input) => {
+    if (input.toUpperCase() == 'Y') {
+        new PREPARE_MODULE([], 'C').__prpPrjct();
 
-            new PREPARE_MODULE(input).traverse(__dirname);
+        RL.close();
+    } else if (input.toUpperCase() == 'N') {
+        RL.close();
+    }
+})
+
+RL.question(colors.bold('Change finding signatures (words) for the finder script [Y/N/C]: '), (answer) => {
+    if (answer.toUpperCase() == 'Y') {
+        RL.question(colors.bold('Enter your custom signatures (words) separatedly by commas: '), (addition) => {
+            const input: string[] = addition.split(',');
+
+            new PREPARE_MODULE(input, 'Y').__traverDir(__dirname);
 
             RL.close();
         });
-    }
-    else {
-        new PREPARE_MODULE(['NO']).traverse(__dirname);
+    } else if (answer.toUpperCase() == 'C') {
+        RL.question(colors.bold('Enter your custom signatures (words) separatedly by commas: '), (addition) => {
+            const input: string[] = addition.split(',');
+
+            new PREPARE_MODULE(input, 'C').__traverDir(__dirname);
+
+            RL.close();
+        });
+    } else if (answer.toUpperCase() == 'N') {
+        new PREPARE_MODULE([], 'N').__traverDir(__dirname);
+
+        RL.close();
     }
 
     RL.close();
