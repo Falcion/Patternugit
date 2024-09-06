c77e05febae00c8363cf83b0e4d74b9bfeb567d1
Falcion <57592842+Falcion@users.noreply.github.com>
2024-09-06 13:58:35 +0300

    git(labels-data): add legacy web scripts to work with labels

---
+++ data/.labels-api.js
@@ -0,0 +1,8 @@
+/**
+ * WARNING: code provided in source below (either link or codelines), this code MAY BE UNSAFE OR UNSTABLE,
+ * use at your own risk.
+ */
+
+/**
+ * https://gist.github.com/Falcion/c600339cff323048561bdf2c7555ecdd
+ */
