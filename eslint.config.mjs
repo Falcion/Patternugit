488127447ae93e801d95e7ca39381ef62779cf83
Falcion <57592842+Falcion@users.noreply.github.com>
2024-11-27 07:25:41 +0300

    chore: another tweak in eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,63 +10,62 @@ import eslintPluginJsonc from 'eslint-plugin-jsonc'
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
-  recommendedConfig: js.configs.recommended,
-  allConfig: js.configs.all
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
 })
 
 export default [
-  ...eslintPluginJsonc.configs['flat/recommended-with-jsonc'],
-  {
-    // Global ignore patterns
-    ignores: [
-      '**/.eslintrc.cjs',
-      '**/database/**/*.js',
-      '**/database/**/*.ts',
-      '**/*.cjs',
-      '**/node_modules/',
-      '**/dist/',
-      '**/prepare_template.js',
-      '**/out/*',
-      '**/*.d.ts'
-    ]
-  },
-  {
-    // JavaScript-specific configuration
-    files: ['**/*.js', '**/*.cjs', '**/*.mjs'],
-    languageOptions: {
-      ecmaVersion: 2020,
-      sourceType: 'module',
-      globals: {
-        ...globals.node,
-        ...globals.browser,
-        ...globals.es2016
-      }
-    }
-  },
-  {
-    // TypeScript-specific configuration
-    files: ['**/*.ts', '**/*.tsx'],
-    plugins: {
-      '@typescript-eslint': typescriptEslint
+    ...eslintPluginJsonc.configs['flat/recommended-with-jsonc'],
+    {
+        // Global ignore patterns
+        ignores: [
+            '**/.eslintrc.cjs',
+            '**/database/**/*.js',
+            '**/database/**/*.ts',
+            '**/*.cjs',
+            '**/node_modules/',
+            '**/dist/',
+            '**/prepare_template.js',
+            '**/out/*',
+            '**/*.d.ts'
+        ]
     },
-    languageOptions: {
-      parser: tsParser,
-      parserOptions: {
-        parser: '@typescript-eslint/parser',
-        project: './tsconfig.json',
-        tsconfigRootDir: __dirname
-      }
+    {
+        // JavaScript-specific configuration
+        files: ['**/*.js', '**/*.cjs', '**/*.mjs'],
+        languageOptions: {
+            ecmaVersion: 2020,
+            sourceType: 'module',
+            globals: {
+                ...globals.node,
+                ...globals.browser,
+                ...globals.es2016
+            }
+        }
     },
-    rules: {
-      ...typescriptEslint.configs.recommended.rules, // Use recommended TypeScript rules
-      '@typescript-eslint/interface-name-prefix': 'off',
-      '@typescript-eslint/explicit-function-return-type': 'off',
-      '@typescript-eslint/explicit-module-boundary-types': 'off',
-      '@typescript-eslint/no-explicit-any': 'warn',
-      '@typescript-eslint/no-non-null-assertion': 'off',
-      '@typescript-eslint/no-var-requires': 'off'
-    }
-  },
-  ...compat.extends('eslint:recommended', 'plugin:@typescript-eslint/recommended')
+    {
+        // TypeScript-specific configuration
+        files: ['**/*.ts', '**/*.tsx'],
+        plugins: {
+            '@typescript-eslint': typescriptEslint
+        },
+        languageOptions: {
+            parser: tsParser,
+            parserOptions: {
+                parser: '@typescript-eslint/parser',
+                project: './tsconfig.json',
+                tsconfigRootDir: __dirname
+            }
+        },
+        rules: {
+            '@typescript-eslint/interface-name-prefix': 'off',
+            '@typescript-eslint/explicit-function-return-type': 'off',
+            '@typescript-eslint/explicit-module-boundary-types': 'off',
+            '@typescript-eslint/no-explicit-any': 'warn',
+            '@typescript-eslint/no-non-null-assertion': 'off',
+            '@typescript-eslint/no-var-requires': 'off'
+        }
+    },
+    ...compat.extends('eslint:recommended', 'plugin:@typescript-eslint/recommended')
 ]
