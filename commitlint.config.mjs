b3b9090a94bf7e26ec4327978dbe7cab0a9e68df
Falcion <57592842+Falcion@users.noreply.github.com>
2025-01-20 09:24:54 +0300

    build(build-ci): append rules in the commitlint's environment

--- commitlint.config.mjs
+++ commitlint.config.mjs
@@ -3,5 +3,9 @@ module.exports = {
   ignores: [
     (message) => /^Repo visualizer: update diagram$/m.test(message),
     (message) => /^Bumps [.+](.+) from .+ to .+.$/m.test(message)
-  ]
+  ],
+  rules: {
+    'header-max-length': async () => [2, 'always', 72],
+    'body-max-length': async () => [0, 'always', 100],
+  },
 }
