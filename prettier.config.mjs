341bfddb6e2c640b1949ec10ae22881d8975adea
Falcion <57592842+Falcion@users.noreply.github.com>
2025-01-20 09:32:13 +0300

    feat(build-ci): remove PRETTIER from super-linter config, append MJS config for PRETTIER in the environment

---
+++ prettier.config.mjs
@@ -0,0 +1,43 @@
+/**
+ * @see https://prettier.io/docs/en/configuration.html
+ * @type {import("prettier").Config}
+ */
+const config = {
+    tabWidth: 2,
+    useTabs: false,
+    bracketSpacing: true,
+    singleQuote: true,
+    endOfLine: 'crlf',
+    trailingComma: 'none',
+    arrowParens: 'always',
+    semi: false,
+    overrides: [
+        {
+            files: '*.md',
+            options: {
+                parser: 'markdown',
+            },
+        },
+        {
+            files: '*.{js,cjs,mjs,jsx}',
+            options: {
+                parser: 'babel',
+            },
+        },
+        {
+            files: '*.{ts,tsx,d.ts}',
+            options: {
+                parser: 'babel-ts',
+            },
+        },
+        {
+            files: '*.{json}',
+            options: {
+                parser: 'jsonc',
+            },
+        },
+    ],
+    printWidth: 100,
+}
+
+export default config;
